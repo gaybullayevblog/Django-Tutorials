16443bafcd95c8b3f883994617cb7bdee668c7e6
gaybullayevblog <gaybullayevblog@gmail.com>
2023-08-12 20:01:44 +0500

    done

--- models/admin.py
+++ models/admin.py
@@ -4,3 +4,6 @@ from .models import *
 
 admin.site.register(Musician)
 admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Author)
+admin.site.register(Book)
--- models/models.py
+++ models/models.py
@@ -16,3 +16,27 @@ class Album(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+  
+class Author(models.Model):
+    name = models.CharField(max_length = 100)
+    desc = models.TextField(max_length = 300)
+
+    def __str__(self):
+        return self.name
+  
+class Book(models.Model):
+    title = models.CharField(max_length = 100)
+    desc = models.TextField(max_length = 300)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
